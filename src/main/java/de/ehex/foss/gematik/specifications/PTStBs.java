6de9cf2d737b18f7e7e3bb967be9866a3e444dea
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2016-09-26 16:28:00 +0200

    Formatting enum constants end token for readable merges

--- src/main/java/de/ehex/foss/gematik/specifications/PTStBs.java
+++ src/main/java/de/ehex/foss/gematik/specifications/PTStBs.java
@@ -996,8 +996,9 @@ public enum PTStBs implements PTStB {
     /**
      * Definition of all partial AFOs of [gemProdT_Kon_PTV2.6.0] required to execute test targeting the LDAP-Proxy.
      */
-    gemProdT_Kon_PTV260_V100_LDAPProxy("gemProdT_Kon_PTV2.6.0 (LDAP-Proxy)", asList(GS_A_3695, GS_A_3804, GS_A_3856, GS_A_3931, GS_A_4386, TIP1_A_4514, TIP1_A_4515, TIP1_A_4518, TIP1_A_4689, TIP1_A_4693, TIP1_A_4696, TIP1_A_4812, TIP1_A_5401, TIP1_A_5516, TIP1_A_5517, TIP1_A_5518, TIP1_A_5519, TIP1_A_5520, TIP1_A_5521, TIP1_A_5568, TIP1_A_5570), asList(GS_A_3695, GS_A_3931, GS_A_4386, TIP1_A_4515, TIP1_A_4693, TIP1_A_4696, TIP1_A_4812));
+    gemProdT_Kon_PTV260_V100_LDAPProxy("gemProdT_Kon_PTV2.6.0 (LDAP-Proxy)", asList(GS_A_3695, GS_A_3804, GS_A_3856, GS_A_3931, GS_A_4386, TIP1_A_4514, TIP1_A_4515, TIP1_A_4518, TIP1_A_4689, TIP1_A_4693, TIP1_A_4696, TIP1_A_4812, TIP1_A_5401, TIP1_A_5516, TIP1_A_5517, TIP1_A_5518, TIP1_A_5519, TIP1_A_5520, TIP1_A_5521, TIP1_A_5568, TIP1_A_5570), asList(GS_A_3695, GS_A_3931, GS_A_4386, TIP1_A_4515, TIP1_A_4693, TIP1_A_4696, TIP1_A_4812)),
 
+    ;
 
     private PTStBs(final String reference, final List<AFO> testAFOs, final List<AFO> nonTestAFOs) {
         assert nonNull(reference) : "There must be a non-null gematik reference identifier!";
