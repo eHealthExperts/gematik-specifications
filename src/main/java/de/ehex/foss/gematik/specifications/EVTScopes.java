79658ee675efd8d5b12896db281a067b8c6d8d81
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2016-10-04 16:03:04 +0200

    Refer to specific errata's AFO instead of errata itself

--- src/main/java/de/ehex/foss/gematik/specifications/EVTScopes.java
+++ src/main/java/de/ehex/foss/gematik/specifications/EVTScopes.java
@@ -1,12 +1,12 @@
 package de.ehex.foss.gematik.specifications;
 
-import static de.ehex.foss.gematik.specifications.Erratas.gemErrata_R1_4_6;
 import static de.ehex.foss.gematik.specifications.PTStBs.gemProdT_CM_KOMLE_PTV1_2_0;
 import static de.ehex.foss.gematik.specifications.PTStBs.gemProdT_FD_KOMLE_PTV1_2_0;
 import static de.ehex.foss.gematik.specifications.PTStBs.gemProdT_FD_VSDM_PTV1_4_0;
 import static de.ehex.foss.gematik.specifications.PTStBs.gemProdT_Intermediaer_VSDM_PTV150_V100;
 import static de.ehex.foss.gematik.specifications.PTStBs.gemProdT_Kon_PTV260_V100_LDAPProxy;
 import static de.ehex.foss.gematik.specifications.PTStBs.gemProdT_X_509_TSP_nonQES_eGK_PTV1_5_0_V1_0_0;
+import static de.ehex.foss.gematik.specifications.gemErrata_R1_4_6.AFOs.ARV_706_3_SPEC_SST_STAMPEL_AFO_0010;
 import static de.ehex.foss.gematik.specifications.gemSpec_Krypt.AFOs.GS_A_4384;
 import static de.ehex.foss.gematik.specifications.gemSpec_Krypt.AFOs.GS_A_4387;
 import static de.ehex.foss.gematik.specifications.gemSpec_PKI.AFOs.GS_A_4642;
@@ -21,6 +21,7 @@ import static de.ehex.foss.gematik.specifications.gemSpec_SST_FD_VSDM.AFOs.VSDM_
 import static de.ehex.foss.gematik.specifications.gemSpec_TSL.AFOs.TIP1_A_5120;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static java.util.Collections.unmodifiableSet;
 
 import java.util.ArrayList;
@@ -48,16 +49,16 @@ public enum EVTScopes implements TestScope {
      * <em>GS_A_4642, GS_A_4643, GS_A_4646, GS_A_4647, GS_A_4648, GS_A_4649, GS_A_4650, GS_A_4655, TIP1_A_5120</em>
      * is/are included into test-scope.
      */
-    OCSP_EGK(asList(gemProdT_X_509_TSP_nonQES_eGK_PTV1_5_0_V1_0_0, gemErrata_R1_4_6), emptyList(), asList(GS_A_4642, GS_A_4643, GS_A_4646, GS_A_4647, GS_A_4648, GS_A_4649, GS_A_4650, GS_A_4655, TIP1_A_5120)),
+    OCSP_EGK(asList(gemProdT_X_509_TSP_nonQES_eGK_PTV1_5_0_V1_0_0), emptyList(), asList(ARV_706_3_SPEC_SST_STAMPEL_AFO_0010, GS_A_4642, GS_A_4643, GS_A_4646, GS_A_4647, GS_A_4648, GS_A_4649, GS_A_4650, GS_A_4655, TIP1_A_5120)),
 
     /**
      * TODO: Put a note why <em>VSDM_A_2323</em> is/are be excluded from testing.
      *
      * TODO: Put a note why <em>GS_A_4384, GS_A_4387</em> is/are included into test-scope.
      */
-    VSDM_FD(asList(gemProdT_FD_VSDM_PTV1_4_0, gemErrata_R1_4_6), asList(VSDM_A_2323), asList(GS_A_4384, GS_A_4387)),
+    VSDM_FD(asList(gemProdT_FD_VSDM_PTV1_4_0), asList(VSDM_A_2323), asList(ARV_706_3_SPEC_SST_STAMPEL_AFO_0010, GS_A_4384, GS_A_4387)),
 
-    INTERMEDIAER_VSDM(asList(gemProdT_Intermediaer_VSDM_PTV150_V100, gemErrata_R1_4_6), emptyList(), emptyList()),
+    INTERMEDIAER_VSDM(asList(gemProdT_Intermediaer_VSDM_PTV150_V100), emptyList(), singletonList(ARV_706_3_SPEC_SST_STAMPEL_AFO_0010)),
 
     ;
 
