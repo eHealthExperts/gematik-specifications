7e6863ad89bbb32f15c73e54ad9c60a45c28a897
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2016-12-02 18:40:35 +0100

    J8Unit into **/AFOs*Test(s)

---
+++ src/test/java/de/ehex/foss/gematik/specifications/gemSpec_Perf/AFOsClassTest.java
@@ -0,0 +1,14 @@
+package de.ehex.foss.gematik.specifications.gemSpec_Perf;
+
+import org.j8unit.runners.J8Unit4;
+import org.junit.runner.RunWith;
+
+@RunWith(J8Unit4.class)
+public final class AFOsClassTest implements AFOsClassTests<AFOs> {
+
+    @Override
+    public final Class<AFOs> createNewSUT() {
+        return AFOs.class;
+    }
+
+}
