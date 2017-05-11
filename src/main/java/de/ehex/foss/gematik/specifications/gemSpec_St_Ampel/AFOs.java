ed3dc82647d76d1505929265107a83f761586455
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2017-05-11 11:28:29 +0200

    Ergänzt: 5 AFOs aus gemSpec_St_Ampel

---
+++ src/main/java/de/ehex/foss/gematik/specifications/gemSpec_St_Ampel/AFOs.java
@@ -0,0 +1,79 @@
+package de.ehex.foss.gematik.specifications.gemSpec_St_Ampel;
+
+import static de.ehex.foss.gematik.specifications.AFOType.MUST;
+import static java.util.Objects.nonNull;
+import de.ehex.foss.gematik.specifications.AFO;
+import de.ehex.foss.gematik.specifications.AFOType;
+
+/**
+ * Enumeration of all &ndash;&nbsp;currently considered&nbsp;&ndash; {@link AFO}s of {@code [gemSpec_St_Ampel]}.
+ *
+ * @author Stefan Gasterst&auml;dt
+ * @since May 11th, 2017
+ */
+public enum AFOs
+implements AFO {
+
+    // TODO: AFO-Typ klären derzeit implizit MUSS-AFO
+    TIP1_A_5993("TIP1-A_5993", "Störungsampel und Client, I_Monitoring_Update, WebService"),
+
+    // TODO: AFO-Typ klären derzeit implizit MUSS-AFO
+    TIP1_A_5996("TIP1-A_5996", "Störungsampel und Client, I_Monitoring_Update, maximale Zeitabweichung zwischen Berichtszeitraum und Nachrichtenübermittelung"),
+
+    // TODO: AFO-Typ klären derzeit implizit MUSS-AFO
+    TIP1_A_5997("TIP1-A_5997", "Nutzer der Störungsampel I_Monitoring_Update, Zeitstempel bei Ausfall/Wiederherstellung"),
+
+    // TODO: AFO-Typ klären derzeit implizit MUSS-AFO
+    TIP1_A_5998("TIP1-A_5998", "Nutzer der Störungsampel I_Monitoring_Update, Zertifikatsprüfung"),
+
+    // TODO: AFO-Typ klären derzeit implizit MUSS-AFO
+    TIP1_A_6003("TIP1-A_6003", "Nutzer der Störungsampel I_Monitoring_Update, eindeutige Zuordnung des Messwertes"),
+
+    ;
+
+    private AFOs(final String afoID, final String title) {
+        this(afoID, title, MUST);
+    }
+
+    private AFOs(final String afoID, final String title, final AFOType level) {
+        assert nonNull(afoID) : "There must be a non-null AFO id!";
+        assert !afoID.isEmpty() : "There must be a non-empty AFO id!";
+        assert nonNull(title) : "There must be a non-null AFO title!";
+        assert !title.isEmpty() : "There must be a non-empty AFO title!";
+        assert nonNull(level) : "There must be a non-null AFO type!";
+
+        this.afoID = afoID;
+        this.title = title;
+        this.level = level;
+    }
+
+    private final String afoID;
+
+    @Override
+    public String getAfoId() {
+        assert nonNull(this.afoID) : "Class invariant violation!";
+        assert !this.afoID.isEmpty() : "Class invariant violation!";
+
+        return this.afoID;
+    }
+
+    private final String title;
+
+    @Override
+    public String getLabel() {
+        assert nonNull(this.title) : "Class invariant violation!";
+        assert !this.title.isEmpty() : "Class invariant violation!";
+
+        return this.title;
+    }
+
+    private final AFOType level;
+
+    @Override
+    public AFOType getType() {
+        assert nonNull(this.level) : "Class invariant violation!";
+
+        return this.level;
+    }
+
+}
