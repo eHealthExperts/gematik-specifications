4bcd9725ab86a1276962290cfab1912bf735fe99
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2017-05-12 10:26:14 +0200

    Removing [gemErrata_R1.4.6]

--- src/main/java/de/ehex/foss/gematik/specifications/EVTScopes.java
+++ src/main/java/de/ehex/foss/gematik/specifications/EVTScopes.java
@@ -6,7 +6,6 @@ import static de.ehex.foss.gematik.specifications.PTStBs.gemProdT_FD_VSDM_PTV1_5
 import static de.ehex.foss.gematik.specifications.PTStBs.gemProdT_Intermediaer_VSDM_PTV150_V100;
 import static de.ehex.foss.gematik.specifications.PTStBs.gemProdT_Kon_PTV260_V100_LDAPProxy;
 import static de.ehex.foss.gematik.specifications.PTStBs.gemProdT_X_509_TSP_nonQES_eGK_PTV1_6_0_V1_2_1;
-import static de.ehex.foss.gematik.specifications.gemErrata_R1_4_6.AFOs.ARV_706_3_SPEC_SST_STAMPEL_AFO_0010;
 import static de.ehex.foss.gematik.specifications.gemSpec_Krypt.AFOs.GS_A_4384;
 import static de.ehex.foss.gematik.specifications.gemSpec_Krypt.AFOs.GS_A_4387;
 import static de.ehex.foss.gematik.specifications.gemSpec_PKI.AFOs.GS_A_4642;
@@ -21,7 +20,6 @@ import static de.ehex.foss.gematik.specifications.gemSpec_SST_FD_VSDM.AFOs.VSDM_
 import static de.ehex.foss.gematik.specifications.gemSpec_TSL.AFOs.TIP1_A_5120;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
-import static java.util.Collections.singletonList;
 import static java.util.Collections.unmodifiableSet;
 
 import java.util.ArrayList;
@@ -49,16 +47,16 @@ public enum EVTScopes implements TestScope {
      * <em>GS_A_4642, GS_A_4643, GS_A_4646, GS_A_4647, GS_A_4648, GS_A_4649, GS_A_4650, GS_A_4655, TIP1_A_5120</em>
      * is/are included into test-scope.
      */
-    OCSP_EGK(gemProdT_X_509_TSP_nonQES_eGK_PTV1_6_0_V1_2_1, emptyList(), asList(ARV_706_3_SPEC_SST_STAMPEL_AFO_0010, GS_A_4642, GS_A_4643, GS_A_4646, GS_A_4647, GS_A_4648, GS_A_4649, GS_A_4650, GS_A_4655, TIP1_A_5120)),
+    OCSP_EGK(gemProdT_X_509_TSP_nonQES_eGK_PTV1_6_0_V1_2_1, emptyList(), asList(GS_A_4642, GS_A_4643, GS_A_4646, GS_A_4647, GS_A_4648, GS_A_4649, GS_A_4650, GS_A_4655, TIP1_A_5120)),
 
     /**
      * TODO: Put a note why <em>VSDM_A_2323</em> is/are be excluded from testing.
      *
      * TODO: Put a note why <em>GS_A_4384, GS_A_4387</em> is/are included into test-scope.
      */
-    VSDM_FD(gemProdT_FD_VSDM_PTV1_5_0_1, asList(VSDM_A_2323), asList(ARV_706_3_SPEC_SST_STAMPEL_AFO_0010, GS_A_4384, GS_A_4387)),
+    VSDM_FD(gemProdT_FD_VSDM_PTV1_5_0_1, asList(VSDM_A_2323), asList(GS_A_4384, GS_A_4387)),
 
-    INTERMEDIAER_VSDM(gemProdT_Intermediaer_VSDM_PTV150_V100, emptyList(), singletonList(ARV_706_3_SPEC_SST_STAMPEL_AFO_0010)),
+    INTERMEDIAER_VSDM(gemProdT_Intermediaer_VSDM_PTV150_V100, emptyList(), emptyList()),
 
     ;
 
